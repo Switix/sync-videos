3b9d56d55402607a279491b972ca679136a3c4dd
Maciej <maciejswitalski7@gmail.com>
2024-07-23 15:07:37 +0200

    Adjusted header to look good on phones

--- frontend/src/component/Header.js
+++ frontend/src/component/Header.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { clearUser,clearAccessToken,clearRefreshToken } from '../redux/slice/authSlice';
+import { clearUser, clearAccessToken, clearRefreshToken } from '../redux/slice/authSlice';
 import { useDispatch, useSelector } from 'react-redux';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 function Header() {
     const user = useSelector(state => state.user);
@@ -9,18 +10,13 @@ function Header() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-    function LoginRegisterLinks() {
-        return (
-            <>
-                <Link to="/login" className="px-3 py-2 text-md font-medium text-white text-center rounded-md bg-green-600 hover:bg-green-700">
-                    Log In
-                </Link>
-                <Link to="/register" className="px-3 py-2 text-md font-medium text-white text-center rounded-md bg-neutral-700 hover:bg-neutral-800">
-                    Sign Up
-                </Link>
-            </>
-        );
-    }
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+
 
     const handleLogout = () => {
         dispatch(clearUser());
@@ -33,35 +29,84 @@ function Header() {
     return (
         <header className="sticky top-0 bg-stone-950 shadow-md z-50">
             <div className="mx-auto py-2 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
-                <Link to="/" className="text-2xl font-bold">
+                <Link to="/" className="text-xl sm:text-2xl font-bold">
                     <span className='text-red-600'>Sync </span>
                     <span className='text-white'>Videos</span>
                 </Link>
-                <nav className="flex space-x-4 items-center">
-                    {registeringUser.username ? (
-                        // Display registeringUser.username if available
-                        <>
-                            <p style={{ color: registeringUser.color }}> {registeringUser.username}</p>
-                            <LoginRegisterLinks />
-                        </>
+                <div className="flex items-center space-x-4">
+                    {registeringUser?.username ? (
+                        <p style={{ color: registeringUser.color }} className="text-sm sm:text-base">{registeringUser.username}</p>
                     ) : user ? (
-                        // Display user.username if user is logged in
                         <>
-                            <p style={{ color: user.userColor }}> {user.username}</p>
-                            {user.role === 'TEMPORARY_USER' ? (
-                                <LoginRegisterLinks />
-                            ) : (
-                                <Link to="/" className="px-3 py-2 text-md font-medium text-white text-center rounded-md bg-red-600 hover:bg-red-700" onClick={handleLogout}>
+                            <p style={{ color: user.userColor }} className="text-sm sm:text-base">{user.username}</p>
+                        </>
+                    ) : null}
+                    <div className="sm:hidden">
+                        <button onClick={toggleMenu} className="text-white focus:outline-none">
+                            {menuOpen ? <FaTimes className="w-6 h-6" /> : <FaBars className="w-6 h-6" />}
+                        </button>
+                    </div>
+                    {!menuOpen && (
+                        <nav className="hidden sm:flex sm:space-x-4 sm:items-center  ">
+
+
+                            {(!registeringUser?.username && !user) || user?.role === 'TEMPORARY_USER' ? (
+                                <>
+                                    <Link to="/login" className="px-3 py-2 text-md font-medium text-white text-center rounded-md bg-green-600 hover:bg-green-700">
+                                        Log In
+                                    </Link>
+                                    <Link to="/register" className="px-3 py-2 text-md font-medium text-white text-center rounded-md bg-neutral-700 hover:bg-neutral-800">
+                                        Sign Up
+                                    </Link>
+                                </>
+                            ) : null}
+                            {user && user?.role !== 'TEMPORARY_USER' && (
+                                <Link to="/" className="block px-3 py-2 text-base text-center text-white font-medium rounded-md bg-red-600 hover:bg-red-700" onClick={handleLogout}>
                                     Logout
                                 </Link>
                             )}
+                        </nav>
+                    )}
+                </div>
+
+            </div>
+
+            {menuOpen && (
+                <nav className="absolute top-11 left-0 w-full bg-stone-950 text-white p-4 space-y-2 z-40 sm:hidden">
+
+
+                    {(!registeringUser?.username && !user) && (
+                        <>
+                            <div className='flex flex-col space-y-2'>
+                                <Link to="/login" className="px-3 py-2 text-md font-medium text-white text-center rounded-md bg-green-600 hover:bg-green-700">
+                                    Log In
+                                </Link>
+                                <Link to="/register" className="px-3 py-2 text-md font-medium text-white text-center rounded-md bg-neutral-700 hover:bg-neutral-800">
+                                    Sign Up
+                                </Link>
+                            </div>
+
                         </>
-                    ) : (
-                        // Display Log In and Sign Up links if user is not logged in
-                        <LoginRegisterLinks />
+                    )}
+                    {user?.role === 'TEMPORARY_USER' && (
+                        <>
+                            <div className='flex flex-col space-y-2'>
+                                <Link to="/login" className="px-3 py-2 text-md font-medium text-white text-center rounded-md bg-green-600 hover:bg-green-700">
+                                    Log In
+                                </Link>
+                                <Link to="/register" className="px-3 py-2 text-md font-medium text-white text-center rounded-md bg-neutral-700 hover:bg-neutral-800">
+                                    Sign Up
+                                </Link>
+                            </div>
+                        </>
+                    )}
+                    {user && user?.role !== 'TEMPORARY_USER' && (
+                        <Link to="/" className="block px-3 py-2 text-base text-white text-center font-medium rounded-md bg-red-600 hover:bg-red-700" onClick={handleLogout}>
+                            Logout
+                        </Link>
                     )}
                 </nav>
-            </div>
+            )}
         </header>
     );
 }
